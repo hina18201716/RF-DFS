1a5eab58704b88f6b7f86adb1ee8e57f89d4a2c6
hina18201716 <hina.551720.s@gmail.com>
2024-08-01 09:54:36 -0600

    log data into file with tab delimiter. This is convertable to cvs file

--- GUI/data.py
+++ GUI/data.py
@@ -1,22 +1,32 @@
+from functools import reduce
+ 
 class DataManagement():
     '''this class contain functions to manage data logging and updating'''
     dataList    = [] # list of data
-    data        = [] # date, azimuth, elevation 
-
-    def __init__( self, userData ):
-        self.dataList = [userData]
-
-    def addNewData( self, date, azimuth, elevation ):
-        userData = [date, azimuth, elevation]
-        self.dataList.append( userData )
+    data        = [] # time, azimuth, elevation 
+    delim = '\t'
 
     def add( self, newData ):
         self.dataList.append(newData)
 
     def delete( self, oldData ):
         self.dataList.remove(oldData)
+    
+    def printData(self):
+        filename = str(self.dataList[0][0])+ ".txt"
+        with open( filename , 'w') as f:
+           for vec in self.dataList:
+             line = reduce(lambda x,y : str(x) + self.delim + str(y), vec)
+             print(line)
+             f.write(line)
+             f.write("\n")   
 
-    def __str__( self ):
-        """"print all information"""
-        return 
+######## example ##########################            
+user = DataManagement()
+user.add(['august1st', 00, 00 ])
+user.add(['august1st', 10, 00 ])
+user.add(['august1st', 20, 00 ])
+user.add(['august1st', 30, 00 ])
+user.printData()
+#############################################
 
